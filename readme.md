9997b4fa2fdd5f6339cb7a7f6e2aabb2f10e1476
yasir <myassir@o2onelabs.com>
2020-04-16 14:41:31 +0530

    Just Commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Nodejs
+
+We assume Nodejs is installed in your system
+
+# Login the open weather system and copy the api key
+ 
+and paste your apikey in apikey variable in server file
+
+# Start the project
+ 1-npm install
+ 2-npm start 
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+const apiKey = '*********************'; //Put your api key here
+
+const port = 8000
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index', {weather: null, error: null}));
+app.post('/', (req, res) => {
+    let city = req.body.city;
+    console.log('inside the body', city);
+    
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  
+    request(url, function (err, response, body) {
+      if(err){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weather = JSON.parse(body)
+        if(weather.main == undefined){
+          res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+          let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+          res.render('index', {weather: weatherText, error: null});
+        }
+    }
+    })
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
